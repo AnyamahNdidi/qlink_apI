2b1caafeccf41c60a0318e22e15723f0309a2bd9
AnyamahNdidi <theo4felix@gmail.com>
2022-05-20 11:12:12 +0100

    socket io downgraded

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 // require("./dbConfig/db");
 const express = require("express");
 const cors = require("cors");
-const http = require('http')
+
 const mongoose = require("mongoose")
 
 const url_online  = "mongodb+srv://gideon:NTp46J2P7Efieni@cluster0.7rupp.mongodb.net/QlinkDataDB?retryWrites=true&w=majority"
@@ -15,10 +15,11 @@ mongoose.connect(url_online).then(()=>{
 })
 
 const port = process.env.PORT || 6905;
+const http = require('http')
 const app = express();
 const server = http.createServer(app)
 const {Server} = require("socket.io");
-// const { on } = require("./Model/UserModel");
+const { on } = require("./Model/UserModel");
 const io = new Server(server, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
@@ -30,10 +31,18 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/api/user", require("./router/UserRouter"));
+app.use("/", require("./router/chatRouter"));
+app.use("/api/user", require("./router/addFriendRouter"));
+app.use("/api/jobs", require("./router/jobRouter"));
+app.use("/api/jobs", require("./router/appliedRouter"));
+app.use("/api/pay", require("./router/payRouter"));
+
 const db = mongoose.connection;
 
 db.on("open", ()=>{
 	const dbConnect = db.collection("chats").watch()
+
 	dbConnect.on("change", (change)=>{
 		console.log(change)
 		if(change.operationType === "insert"){
@@ -52,12 +61,7 @@ db.on("open", ()=>{
 })
 
 // app.use(express.json())
-app.use("/api/user", require("./router/UserRouter"));
-app.use("/", require("./router/chatRouter"));
-app.use("/api/user", require("./router/addFriendRouter"));
-app.use("/api/jobs", require("./router/jobRouter"));
-app.use("/api/jobs", require("./router/appliedRouter"));
-app.use("/api/pay", require("./router/payRouter"));
+
 
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
